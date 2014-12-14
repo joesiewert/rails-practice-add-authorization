26f5cb709c594e3651a4c890cf5b9378d30d9255
joesiewert <joe.siewert@gmail.com>
2014-12-13 17:14:41 -0700

    Completed exercise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :require_login
 
   def current_student
-    Student.find_by_id(session[:student_id])
+    if session[:student_id]
+      Student.find_by_id(session[:student_id])
+    end
   end
 
   helper_method :current_student
+
+  def require_login
+    unless current_student
+      redirect_to login_path
+    end
+  end
 
 end
